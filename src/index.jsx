83334d7c097429a67908b76d75aff6d80b569e49
alstjr5949 <alstjr5949@gmail.com>
2022-06-14 01:15:14 +0900

    basename

--- src/index.jsx
+++ src/index.jsx
@@ -25,7 +25,7 @@ const GlobalStyle = createGlobalStyle`
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <Header />
       <App />
